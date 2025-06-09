eeef68d39f332a7a1c97b86c36ac5bea9f13ef84
Даниил <lilwasd23@gmail.com>
2025-06-09 10:55:50 +0300

    dotfiles not hidden in neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,9 +7,19 @@ return {
     "MunifTanjim/nui.nvim",
   },
   keys = {
-    { "<C-t>", ":Neotree toggle<CR>", desc = "NeoTree" },  -- Перенесено в `keys`
+    { "<C-t>", ":Neotree toggle<CR>", desc = "NeoTree" },
   },
-  config = function()
-    require("neo-tree").setup({})  -- Обязательный вызов setup()
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,         -- Показывать скрытые файлы (они будут "приглушены")
+        hide_dotfiles = false,  -- Не скрывать файлы, начинающиеся с точкой
+        hide_gitignored = false,-- Не скрывать файлы из .gitignore
+        hide_hidden = false,    -- Не скрывать скрытые файлы (важно!)
+      },
+    },
+  },
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
   end,
 }
